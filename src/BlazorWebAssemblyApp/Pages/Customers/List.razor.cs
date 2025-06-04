7dde3a144573e3f651ad10dbb17e3e67f7a22831
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-06-04 16:15:27 +0200

    Zastosowanie klas częściowych partial class

---
+++ src/BlazorWebAssemblyApp/Pages/Customers/List.razor.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace BlazorWebAssemblyApp.Pages.Customers;
+
+public partial class List
+{
+    private string message = "Hello Customers!";
+
+    private List<Customer>? customers;
+
+    [Inject] // Atrybut pozwalający na wstrzykiwanie zależności
+    private HttpClient http { get; set; } // Property
+
+    protected override async Task OnInitializedAsync()
+    {
+        customers = await http.GetFromJsonAsync<List<Customer>>("https://localhost:7162/api/customers");
+    }
+}
+
+
