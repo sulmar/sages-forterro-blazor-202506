ba48cd3f0293be0926f67837dd7fcefc307c8d1f
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-06-05 07:58:45 +0200

    Dodanie listy produktów

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -20,6 +20,10 @@ builder.Services.AddSingleton<IEnumerable<Customer>>(sp =>
 
 });
 
+builder.Services.AddSingleton<Faker<Product>, ProductFaker>();
+builder.Services.AddSingleton<IProductRepository, InMemoryProductRepository>();
+builder.Services.AddSingleton<IEnumerable<Product>>(sp => sp.GetRequiredService<Faker<Product>>().Generate(20));
+
 builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
 {
     policy.WithOrigins("https://localhost:7291", "http://localhost:5076").WithMethods("GET").AllowAnyHeader();
@@ -35,6 +39,7 @@ app.UseHttpsRedirection();
 
 app.MapGet("/", () => "Hello Api!");
 app.MapGet("api/customers", async (ICustomerRepository repository) => await repository.GetAllAsync());
+app.MapGet("api/products", async (IProductRepository repository) => await repository.GetAllAsync());
 
 app.Run();
 
