d8b23d28d87c7f8c18ad102165c489ed55662949
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-06-05 09:49:10 +0200

    Dodanie listy produktów

--- src/BlazorWebAssemblyApp/Pages/Customers/List.razor.cs
+++ src/BlazorWebAssemblyApp/Pages/Customers/List.razor.cs
@@ -1,22 +1,26 @@
-﻿using Domain.Models;
+﻿using Domain.Abstractions;
+using Domain.Models;
 using Microsoft.AspNetCore.Components;
 using System.Net.Http.Json;
 
 namespace BlazorWebAssemblyApp.Pages.Customers;
 
-public partial class List
+
+public partial class List(HttpClient http) // Primary Constructor
 {
     private string message = "Hello Customers!";
 
     private List<Customer>? customers;
 
-    [Inject] // Atrybut pozwalający na wstrzykiwanie zależności
-    private HttpClient http { get; set; } // Property
+   // [Inject] // Atrybut pozwalający na wstrzykiwanie zależności
+   // private readonly HttpClient http { get; set; } // Property
 
     protected override async Task OnInitializedAsync()
-    {
+    {        
         customers = await http.GetFromJsonAsync<List<Customer>>("https://localhost:7162/api/customers");
     }
+
+    
 }
 
 
