b8481167ed101c520a8897b8805e4cb0c122c350
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-06-06 14:39:00 +0200

    Dodanie formularza i walidacji w oparciu o atrybuty

--- src/Domain/Models/Customer.cs
+++ src/Domain/Models/Customer.cs
@@ -1,10 +1,20 @@
-﻿namespace Domain.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Models;
 
 public class Customer : BaseEntity
 {
+    [Required]
+    [MinLength(3)]
+    [MaxLength(10)]
     public required string Name { get; set; }
+
+    [Required, Length(5, 30), EmailAddress]
     public required string Email { get; set; }
+
     public string? HashedPassword { get; set; }
+    [Compare(nameof(HashedPassword))]
     public string? ConfirmedPassword { get; set; }
+
     public bool IsDeleted { get; set; }
 }
