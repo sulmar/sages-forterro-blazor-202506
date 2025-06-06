f8f9c614e8f07cb798768d200f1dabc17cc0efeb
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-06-06 15:12:35 +0200

    Dodanie walidacji za pomocą biblioteki Blazored.FluentValidation

---
+++ src/Domain/Validators/CustomerValidator.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Models;
+using FluentValidation;
+
+namespace Domain.Validators;
+
+
+// dotnet add package FluentValidation
+
+public class CustomerValidator : AbstractValidator<Customer>
+{
+    public CustomerValidator()
+    {
+        RuleFor(p => p.Name).NotEmpty().Length(3, 10);
+        RuleFor(p=>p.Email).EmailAddress().NotEmpty().Length(5, 30);
+        RuleFor(p => p.ConfirmedPassword).Equal(p => p.HashedPassword);
+    }
+}
