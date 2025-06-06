57685f310865a52cd81e42817f8d7b897f253723
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-06-06 16:55:23 +0200

    Dodanie geolokacji jako przykładu wywołania funkcji Blazor z JS

---
+++ src/BlazorWebAssemblyApp/wwwroot/js/geolocation.js
@@ -0,0 +1,24 @@
+﻿window.getLocation = (dotnet) => {
+
+    if (!navigator.geolocation) {
+        console.warn("Geolokacja niedostępna.");
+        return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+        (position) => {
+            const lat = position.coords.latitude;
+            const lng = position.coords.longitude;
+
+            console.log(lat);
+            console.log(lng);
+
+            dotnet.invokeMethodAsync("OnLocationReceived", lat, lng);
+        },
+
+        (error) => {
+            console.error(error);
+        }
+    )
+    
+}
